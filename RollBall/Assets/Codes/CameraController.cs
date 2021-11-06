95dc53c375e36fdf55f82afe3801225ca294d9b9
Vasco-PC <kravchuk.vasilii@yandex.ru>
2021-11-06 16:39:00 +0300

    Camera
    Added different camera speed

--- RollBall/Assets/Codes/CameraController.cs
+++ RollBall/Assets/Codes/CameraController.cs
@@ -5,6 +5,9 @@ namespace Kravchuk
     public sealed class CameraController
     {
         private float _speed = 5f;
+        private float _speedUpMultiplier = 3f;
+        private float _speedMultiplier = 1f;
+        private float _distanceToSpeedChange = 10f;
         private Vector3 _offset = new Vector3(0f, 6f, -4f);
 
         public Camera MainCamera { get; set; }
@@ -15,7 +18,12 @@ namespace Kravchuk
         /// <param name="playerPosition"></param>
         public void LetMove(Vector3 playerPosition)
         {
-            MainCamera.transform.position = Vector3.MoveTowards(MainCamera.transform.position, playerPosition + _offset, _speed * Time.deltaTime);
+            if (Vector3.Magnitude(playerPosition - MainCamera.transform.position) < _distanceToSpeedChange)
+                _speedMultiplier = 1;
+            else
+                _speedMultiplier = _speedUpMultiplier;
+
+            MainCamera.transform.position = Vector3.MoveTowards(MainCamera.transform.position, playerPosition + _offset, _speed * _speedMultiplier * Time.deltaTime);
         }
     }
 }
